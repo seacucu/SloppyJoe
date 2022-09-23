b4f911517c1801898bd319be76bca303d2f5a539
Chloe Lu <luyisien@gmail.com>
2022-09-24 05:32:09 +0800

    fix formats (randon price wip)

--- SloppyJoe/MenuItem.cs
+++ SloppyJoe/MenuItem.cs
@@ -4,39 +4,40 @@ namespace SloppyJoe
 {
     internal class MenuItem
     {
-       // Field
+        // Field
         // 蛋白質
-        public string[] Proteins = 
+        public string[] Proteins =
             { "燒肉", "臘腸", "燻雞", "火腿", "燻肉", "豆腐" };
+
         // 配料
-        public string[] Condiments = 
+        public string[] Condiments =
             { "酸黃瓜", "番茄", "蜂蜜芥末", "起司", "泡菜", "塔塔" };
+
         // 麵包
-        public string[] Bread = 
+        public string[] Bread =
             { "小麥麵包", "黑麥麵包", "白麵包", "燕麥麵包", "麵包捲" };
+
         // 描述
         public string Desc = string.Empty;
+
         // 價格
         public string Price = string.Empty;
 
-       // Instantiation
+        // Instantiation
         public static Random rand = new Random();
 
-       // Method
+        // Method
         public void Generate()
         {
-            // 三種成分各隨機取出一項
+            // 三種成分各隨機取出一項並生成描述
             string rdProtein = Proteins[rand.Next(Proteins.Length)];
             string rdCondiments = Condiments[rand.Next(Condiments.Length)];
-            string rdBread = Bread[rand.Next(Bread.Length)];            
-
-            decimal buck = rand.Next(2, 5);
-            decimal cent = rand.Next(1, 98);
-            decimal totalprice = buck + (cent * 0.01M);
-
-            // 生成描述、價格
+            string rdBread = Bread[rand.Next(Bread.Length)];
             Desc = $"{rdCondiments}{rdProtein}{rdBread}";
-            Price = totalprice.ToString("c");
+
+            // 生成價格
+            int totalprice = rand.Next(100,200);
+            Price = $"NT${totalprice}";
         }
     }
 }
