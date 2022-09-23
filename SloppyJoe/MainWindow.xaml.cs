413ab43c798f009765e82ecc20439a10a9e4d9e4
Chloe Lu <luyisien@gmail.com>
2022-09-24 05:07:11 +0800

    Add project files.

---
+++ SloppyJoe/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+
+namespace SloppyJoe
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MakeTheMenu();
+        }
+
+        private void MakeTheMenu()
+        {
+            // 處理菜單項目 #1-5
+            MenuItem[] menuItems = new MenuItem[5];
+            for (int i = 0; i < 5; i++)
+            {
+                menuItems[i] = new MenuItem();
+                if (i >= 3)
+                {
+                    menuItems[i].Bread = new string[] 
+                    { "奶油貝果", "洋蔥貝果", "黑麥貝果", "鮮奶貝果" };
+                }
+                menuItems[i].Generate();
+            }
+            // 將文字加入 Textblock #1-5
+            item1.Text = menuItems[0].Desc; price1.Text = menuItems[0].Price;
+            item2.Text = menuItems[1].Desc; price2.Text = menuItems[0].Price;
+            item3.Text = menuItems[2].Desc; price3.Text = menuItems[0].Price;
+            item4.Text = menuItems[3].Desc; price4.Text = menuItems[0].Price;
+            item5.Text = menuItems[4].Desc; price5.Text = menuItems[0].Price;
+
+            // 處理菜單項目 #6
+            MenuItem itemSpecial = new MenuItem()
+            {
+                Proteins = new string[] { "澳洲火腿", "蘑菇肉排", "義式肉腸" },
+                Bread = new string[] { "鮮奶麵包捲", "玉米餅", "皮塔餅" },
+                Condiments = new string[] { "第戎芥末", "新鮮山葵", "大骨原汁" }
+            };
+            itemSpecial.Generate();
+            // 將文字加入Textblock #6
+            item6.Text = itemSpecial.Desc;
+            price6.Text = itemSpecial.Price;
+
+            // 處理加蛋項目 #6
+            MenuItem itemAddEgg = new MenuItem();
+            itemAddEgg.Generate();
+            addEgg.Text = $"加蛋加 {itemAddEgg.Price} 元";
+        }
+    }
+}
---
+++ SloppyJoe/MenuItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SloppyJoe
+{
+    internal class MenuItem
+    {
+       // Field
+        // 蛋白質
+        public string[] Proteins = 
+            { "燒肉", "臘腸", "燻雞", "火腿", "燻肉", "豆腐" };
+        // 配料
+        public string[] Condiments = 
+            { "酸黃瓜", "番茄", "蜂蜜芥末", "起司", "泡菜", "塔塔" };
+        // 麵包
+        public string[] Bread = 
+            { "小麥麵包", "黑麥麵包", "白麵包", "燕麥麵包", "麵包捲" };
+        // 描述
+        public string Desc = string.Empty;
+        // 價格
+        public string Price = string.Empty;
+
+       // Instantiation
+        public static Random rand = new Random();
+
+       // Method
+        public void Generate()
+        {
+            // 三種成分各隨機取出一項
+            string rdProtein = Proteins[rand.Next(Proteins.Length)];
+            string rdCondiments = Condiments[rand.Next(Condiments.Length)];
+            string rdBread = Bread[rand.Next(Bread.Length)];            
+
+            decimal buck = rand.Next(2, 5);
+            decimal cent = rand.Next(1, 98);
+            decimal totalprice = buck + (cent * 0.01M);
+
+            // 生成描述、價格
+            Desc = $"{rdCondiments}{rdProtein}{rdBread}";
+            Price = totalprice.ToString("c");
+        }
+    }
+}
